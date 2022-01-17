ce258e0ed95ce34c9efff4c6790a3294ca1f1f4a
Alexander <alexndegwa46@gmail.com>
2022-01-17 14:51:02 +0300

    generated repository component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 
 
@@ -15,6 +16,7 @@ import { UserComponent } from './user/user.component';
   declarations: [
     AppComponent,
     UserComponent,
+    RepositoryComponent,
   
     
     
